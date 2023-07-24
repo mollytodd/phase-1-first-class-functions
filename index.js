e5ceb38f78c65c562496635a5698d4232de7a29b
Molly Todd <mollyjt89@gmail.com>
2023-07-23 18:17:18 -0700

    redid for july 31 cohort

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
-function receivesAFunction(callback) 
-{
-    callback();
+let callback = function () {
+  return "inner function";
+};
+
+function receivesAFunction(callback) {
+  // Call the callback function here
+  callback();
+  return "THIS IS A CALLBACK FUNCTION";
 }
 
+// Call receivesAFunction and pass the callback function to it
+receivesAFunction(callback);
+
+
 function returnsANamedFunction() {
-    const namedFunction = function functionName() {
-     
-    };
-    return namedFunction;
-  }
+  let namedFunction = function innerFunction() {
+    return "This is a named function";
+  };
+  return namedFunction;
+}
+
+let myNamedFunction = returnsANamedFunction();
 
 
-  function returnsAnAnonymousFunction() {
-    return function() {
-   
-    };
-  }
+let result = myNamedFunction();
+console.log(result);
+
+
+function returnsAnAnonymousFunction() {
+  let anonymousFunction = function() {
+    console.log("This is an anonymous function");
+  };
+
+ 
+  return anonymousFunction;
+}
+
+let myAnonymousFunction = returnsAnAnonymousFunction();
+
+
+myAnonymousFunction();
+
+
